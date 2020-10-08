deaccbaf2cccb54d1f02bd95e7503a687fe7ad07
japiirainen <joona.piirainen@gmail.com>
2020-10-08 20:53:44 +0300

    prettier

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,21 @@ import path from 'path'
 const pathToFile = path.join(__dirname, '..', 'test.json')
 
 const getJSON = async (path: string) => {
-  try {
-    const fileContents = await fs.readFile(path)
-    return fileContents.toString()
-  } catch (e) {
-    console.error('error', e)
-  }
-  return ''
+   try {
+      const fileContents = await fs.readFile(path)
+      return fileContents.toString()
+   } catch (e) {
+      console.error('error', e)
+   }
+   return ''
 }
 
 const makeNewJson = async () => {
-  const {array} = await getJSON(pathToFile).then(JSON.parse)
-  return await fs.writeFile(
-    path.join(__dirname, '..', 'some.json'),
-    JSON.stringify(array.reduce((acc: number, x: number) => acc * x))
-  )
+   const { array } = await getJSON(pathToFile).then(JSON.parse)
+   return await fs.writeFile(
+      path.join(__dirname, '..', 'some.json'),
+      JSON.stringify(array.reduce((acc: number, x: number) => acc * x))
+   )
 }
 
 console.log('lol')
